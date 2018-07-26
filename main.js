24e6aa9f80274c1ecfb2d26e88711aca63d95d16
Luiz Campedelli <luiz.campedelli@fbiz.com.br>
2018-07-26 11:21:20 -0300

    working with column control and tab now

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var css = '.append-component-name { position:relative;text-align:center;z-index:9999;font-size:20px;background:rgba(0,120,255,.7);color:black;font-weight:700;padding:10px}',
+var css = '.cmp-nm { height: 60px; margin-bottom: -60px; text-align: center;position:relative;font:20px Verdana;background:rgba(0,0,0,0.7);color:#fff;font-weight:700;width:100%;display:flex;align-items:center;justify-content:center; z-index:9999;} .cmp-nm span { font-size: 10px;}',
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
     style.type = 'text/css';
@@ -10,13 +10,20 @@ if (style.styleSheet)
 
 head.appendChild(style);
 
-$('.component').each(function(index){
-    var componentName = $(this).attr("data-role");
+$('.component-wrapper').each(function(index){
+    var componentName = $(this).children("div").attr("data-role"),
+    columnControl = '';
+
+    if ( $(this).children("div").hasClass("c-column-control") ) {
+      componentName = "column-control";
+    }
 
     $(this).mouseenter(function() {
-        $(this).css('border','2px solid #0f83d5').append('<div class="append-component-name">' + componentName + '</div>');
+        if ( $(this).parents().hasClass("c-column-control") ) {
+          columnControl = "<br><span>(inside column-control)</span>";
+        }
+        $(this).prepend('<div class="cmp-nm"><p>' + componentName + columnControl + '</p></div>');
     }).mouseleave(function() {
-        $(this).css('border','none');
-        $(".append-component-name").remove();
+        $(".cmp-nm").remove();
     });
 });
