9d8785c604272a45b890f82151392f5e49236efc
Luiz Campedelli <luiz.campedelli@fbiz.com.br>
2018-07-26 16:35:04 -0300

    add element outline and capitalize for component name

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var css = '.cmp-nm { height: 60px; margin-bottom: -60px; text-align: center;position:relative;font:20px Verdana;background:rgba(0,0,0,0.7);color:#fff;font-weight:700;width:100%;display:flex;align-items:center;justify-content:center; z-index:9999;} .cmp-nm span { font-size: 10px;}',
+var css = '.cmp-nm { text-transform:capitalize; height: 60px; margin-bottom: -60px; text-align: center;position:relative;font:18px Arial;background:rgba(0,0,0,0.7);color:#fff;font-weight:700;width:100%;display:flex;align-items:center;justify-content:center; z-index:9999;} .cmp-nm span { font-size: 12px;}',
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
     style.type = 'text/css';
@@ -12,18 +12,26 @@ head.appendChild(style);
 
 $('.component-wrapper').each(function(index){
     var componentName = $(this).children("div").attr("data-role"),
-    columnControl = '';
+    columnControl = '',
+    cmptName;
 
     if ( $(this).children("div").hasClass("c-column-control") ) {
-      componentName = "column-control";
+      componentName = "column control";
+    }
+
+    if ( $(this).children("div").hasClass("c-html-injector__wrapper") ) {
+      componentName = "code snippet";
     }
 
     $(this).mouseenter(function() {
+        cmptName = componentName.replace(/-/g, " ");
         if ( $(this).parents().hasClass("c-column-control") ) {
-          columnControl = "<br><span>(inside column-control)</span>";
+          columnControl = "<br><span>(inside column control)</span>";
         }
-        $(this).prepend('<div class="cmp-nm"><p>' + componentName + columnControl + '</p></div>');
+        $(this).css('outline','3px solid rgba(0, 0, 0, .7)');
+        $(this).prepend('<div class="cmp-nm"><p>' + cmptName + columnControl + '</p></div>');
     }).mouseleave(function() {
+        $(this).css('outline','none');
         $(".cmp-nm").remove();
     });
 });
