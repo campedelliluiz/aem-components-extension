cdaddd4e86165854ac4cd63681afa572f3ca0a86
Luiz Campedelli <luiz.campedelli@fbiz.com.br>
2018-08-07 15:27:15 -0300

    loader add

--- content.js
+++ content.js
@@ -1,29 +1,36 @@
 if ($('.component-wrapper').length) {
+
+	$('body').prepend('<div class="loadaem"><img src="https://i.imgur.com/uv7qrvr.gif" /></div>');
+
 	$('.component-wrapper').each(function(index){
-	    var componentName = $(this).children("div").attr("data-role"),
-	    columnControl = '',
-	    cmptName;
+		var componentName = $(this).children("div").attr("data-role"),
+		columnControl = '',
+		cmptName;
 
-	    if ( $(this).children("div").hasClass("c-column-control") ) {
-	      componentName = "column control";
-	    }
+		if ( $(this).children("div").hasClass("c-column-control") ) {
+		  componentName = "column control";
+		}
 
-	    if ( $(this).children("div").hasClass("c-html-injector__wrapper") ) {
-	      componentName = "code snippet";
-	    }
+		if ( $(this).children("div").hasClass("c-html-injector__wrapper") ) {
+		  componentName = "code snippet";
+		}
 
-	    $(this).mouseenter(function() {
-	        cmptName = componentName.replace(/-/g, " ");
-	        if ( $(this).parents().hasClass("c-column-control") ) {
-	          columnControl = "<br><span>(inside column control)</span>";
-	        }
-	        $(this).css('outline','3px solid rgba(0, 0, 0, .7)');
-	        $(this).prepend('<div class="cmp-nm"><p>' + cmptName + columnControl + '</p></div>');
-	    }).mouseleave(function() {
-	        $(this).css('outline','none');
-	        $(".cmp-nm").remove();
-	    });
+		$(this).mouseenter(function() {
+			cmptName = componentName.replace(/-/g, " ");
+			if ( $(this).parents().hasClass("c-column-control") ) {
+			  columnControl = "<br><span>(inside column control)</span>";
+			}
+			$(this).css('outline','3px solid rgba(0, 0, 0, .7)');
+			$(this).prepend('<div class="cmp-nm"><p>' + cmptName + columnControl + '</p></div>');
+		}).mouseleave(function() {
+			$(this).css('outline','none');
+			$(".cmp-nm").remove();
+		});
 	});
+
+	setTimeout(function(){
+	  $(".loadaem").remove();
+	}, 1500);
 } else {
 	alert('This is not a Adobe Classic site');
 }
